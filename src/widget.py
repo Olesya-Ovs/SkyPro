3c79f6da24215e6db2a5da5e44c978a5e2a1109e
olesya ovs <motakuarifuka@gmail.com>
2025-01-18 14:57:44 +0500

    add get_date

--- src/widget.py
+++ src/widget.py
@@ -12,5 +12,16 @@ def mask_account_card(account_card_number: str) -> str:
     return f"{" ".join(list_of_account_card_numbers[:-1])} {account_card_number_mask}"
 
 
+def get_date(date: str) -> str:
+    """меняет формат даты, убирая лишнее"""
+    day = date[8:10]
+    month = date[5:7]
+    year = date[:4]
+    return f"{day}.{month}.{year}"
+
+
 my_account_card_number = mask_account_card("Счет 73654108430135874305")
 print(my_account_card_number)
+
+new_date = get_date("2024-03-11T02:26:18.671407")
+print(new_date)
